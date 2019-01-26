b9fb741cb948704befca04a29e5958fb67e0be16
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-01-26 22:49:32 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(x = 10)
+  n = 10
+  while n >= 1
+  puts "#{n} SECOND(S)!"
+  n -= 1
+end
+return "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(x = 10)
+  sleep 5
+  n = 10
+  while n >= 1
+  puts "#{n} SECOND(S)!"
+  n -= 1
+end
+return "HAPPY NEW YEAR!"
 end
